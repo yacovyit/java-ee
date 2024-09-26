1c12298decd8aa79a1c1063a298c9b3fdda8ebb2
yacov yitzhak <yacovyidev@gmail.com>
2024-09-26 07:56:20 +0300

    add new project jpa-hibernate-example

---
+++ examples/jpa-hibernate-mssql/src/main/java/org/example/App.java
@@ -0,0 +1,33 @@
+package org.example;
+
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class App {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("hibernate");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+
+        transaction.begin();
+
+        // Here is our code ....
+        System.out.println("In our transaction ...");
+
+
+        Book book = new Book(1L, "Harry Potter");
+        entityManager.persist(book);
+
+        transaction.commit();
+        entityManager.close();
+    }
+}
