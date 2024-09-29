af0194e8c73d4ea46e326475ead55f23abfbe62f
yacov yitzhak <yacovyidev@gmail.com>
2024-09-29 17:54:21 +0300

    add comment on book operations

--- java/jpa-hibernate/jpa-hibernate-fundamental/src/main/java/org/example/crudbook/Main.java
+++ java/jpa-hibernate/jpa-hibernate-fundamental/src/main/java/org/example/crudbook/Main.java
@@ -22,32 +22,28 @@ public class Main {
             // Here is our code ....
             System.out.println("In our transaction ...");
 
+            // persist new books
             bookRepository.persist(new Book(1L, "1984", "George Orwell's 1984", 328, 19.99f, "978-0415249353"));
             bookRepository.persist(new Book(2L, "Moby-Dick, or The Whale", "Herman Melville's 1851", 150, 12.99f, "978-04515"));
 
+            // find books by id
             Book book1984 = bookRepository.find(1L);
             Book bookMobyDick = bookRepository.find(2L);
 
             System.out.println("Found book: " + book1984);
             System.out.println("Found book: " + bookMobyDick);
 
-            // update price
+            // update book price
             bookMobyDick.setPrice(24.99f);
             //bookRepository.update(bookMobyDick);
 
-            //bookRepository.remove(book1984);
+            // remove book by entity or by id
+            bookRepository.remove(book1984);
             bookRepository.removeById(book1984.getId());
 
+            // find book by id again to check if it was removed
             book1984 = bookRepository.find(1L);
             System.out.println("After removal, book deleted: " + (book1984== null));
-
-
-
-
-
-
-
-
         }
     }
 }
